bb9bbb947f474d30345446e8bc04193c44714adb
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-04-29 20:04:29 +0200

    Add comments to clarify

--- app/Main.hs
+++ app/Main.hs
@@ -9,19 +9,24 @@ import           System.ZMQ4.Monadic
 
 main :: IO ()
 main = runZMQ $ do
-    -- Socket to talk to clients
+    -- Socket to talk to individual chat clients
     responder <- socket Rep
     bind responder "tcp://*:5555"
 
+    -- Socket to publish new messages to all chat clients
     publisher <- socket Pub
     bind publisher "tcp://*:6666"
 
     forever $ do
+        -- Get new message from a client
         buffer <- receive responder
         -- FIXME: Convert ByteString to String
         -- liftIO $ do
         --     putStrLn (B.append (B.pack "Received: ") buffer)
+
+        -- Acknowledge the message.
         send responder [] "ACK"
 
+        -- Publish the message for all clients to see, on the topic/channel "A"
         send publisher [SendMore] "A"
         send publisher [] buffer
