d6446a4eae482075119f33dbc253aeb4432f2bc9
Dennis Kristiansen <denniskristiansen@protonmail.com>
2021-04-29 19:54:58 +0200

    Try to replicate rust server

--- app/Main.hs
+++ app/Main.hs
@@ -1,22 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 
--- Hello World server
-
 module Main where
 
-import Control.Concurrent
-import Control.Monad
-import System.ZMQ4.Monadic
+import           Control.Concurrent
+import           Control.Monad
+import qualified Data.ByteString.Char8 as B
+import           System.ZMQ4.Monadic
 
 main :: IO ()
 main = runZMQ $ do
     -- Socket to talk to clients
-    responder <- socket Router
+    responder <- socket Rep
     bind responder "tcp://*:5555"
+
+    publisher <- socket Pub
+    bind publisher "tcp://*:6666"
 
     forever $ do
         buffer <- receive responder
-        liftIO $ do
-            putStrLn "Received Hello"
-            threadDelay 1000000 -- Do some 'work'
-        send responder [] "World"
+        -- FIXME: Convert ByteString to String
+        -- liftIO $ do
+        --     putStrLn (B.append (B.pack "Received: ") buffer)
+        send responder [] "ACK"
+
+        send publisher [] buffer
