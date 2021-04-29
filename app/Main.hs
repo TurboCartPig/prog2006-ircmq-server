16546cf1edae54555e65e5b8c20016f1c8b702e7
Salvador Bascunan <sobascun@stud.ntnu.no>
2021-04-29 18:53:46 +0200

    added haskell-server

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- Hello World server
+
 module Main where
 
-import Lib
+import Control.Concurrent
+import Control.Monad
+import System.ZMQ4.Monadic
 
 main :: IO ()
-main = someFunc
+main = runZMQ $ do
+    -- Socket to talk to clients
+    responder <- socket Router
+    bind responder "tcp://*:5555"
+
+    forever $ do
+        buffer <- receive responder
+        liftIO $ do
+            putStrLn "Received Hello"
+            threadDelay 1000000 -- Do some 'work'
+        send responder [] "World"
