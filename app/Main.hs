f4cfc11f0f973ee7cb4ff4054c2ef3a77d8b4aa8
Sebastian Lindtvedt <sebaskl@stud.ntnu.no>
2021-05-03 19:57:39 +0200

    Added support for messageType 'RequestMembers'

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,10 @@ data MessageType
   = Hello { name :: String, channel :: String }
   -- | A text message sent from a client to all other clients.
   | Message { name :: String, channel :: String, content :: String }
+  -- | A request to see all members in a given channel.
+  | RequestMembers { channel :: String }
+  -- | A response of all members present in a given channel
+  | ResponseMembers { members :: [String] }
   -- | Final message from a client, notifying the server, that the client is disconnecting.
   | Goodbye { name :: String, channel :: String }
   deriving(Generic, Show)
@@ -42,17 +46,23 @@ main = runZMQ $ do
     forever $ do
         -- Get new message from a client
         buffer <- receive responder
-        send responder [] "ACK"
-        
+
+
         let json = decodeStrict buffer :: Maybe MessageType
 
         case json of
-          Just (Hello   name ch) -> liftIO $ insertChannelParticipant channels name ch
+          Just (Hello   name ch) -> do
+            send responder [] "ACK"
+            liftIO $ insertChannelParticipant channels name ch
           Just (Message name ch content) -> do
+            send responder [] "ACK"
             liftIO (putStrLn $ name ++ ": " ++ content)
-            -- Publish the message for all clients to see, on the topic/channel "A"
+            -- Publish the message for all clients to see, on the topic/channel 'ch'
             send publisher [SendMore] (B.pack ch)
             send publisher [] (B.pack $ name ++ ": " ++ content)
+          Just (RequestMembers ch) -> do
+            participants <- liftIO (fetchChannelParticipants channels ch)
+            send responder [] (B.pack (show $ encode (ResponseMembers {members = participants})))
           _ -> liftIO (putStrLn "NOT A MESSAGE")
 
 
@@ -70,7 +80,14 @@ insertChannelParticipant (ChannelParticipants cp) name ch = do
       let channels' = Map.insert ch [name] channels
       putMVar cp channels'
       putStrLn ("New participant added to the new channel: " ++ ch)
-  
+
+fetchChannelParticipants :: ChannelParticipants -> String -> IO [String]
+fetchChannelParticipants (ChannelParticipants cp) ch = do
+  channels <- takeMVar cp
+  let res = Map.lookup ch channels
+  case res of
+    Just chn -> return chn
+    Nothing  -> return []
   
 newChannelMap :: IO ChannelParticipants 
 newChannelMap = do 
