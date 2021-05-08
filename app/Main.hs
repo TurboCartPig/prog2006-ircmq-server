ede26de4e9330e62c395d6439cb8ba38e0f5b62e
Sebastian Lindtvedt <sebaskl@stud.ntnu.no>
2021-05-08 12:42:23 +0200

    Changed the channels of which the channellist gets broadcasted to

--- app/Main.hs
+++ app/Main.hs
@@ -56,7 +56,7 @@ main = runZMQ $ do
             channels <- liftIO (fetchAllChannelNames channels)
             send publisher [SendMore] (B.pack channel)
             send publisher [] (C.toStrict . encode $ ResponseMembers {members})
-            send publisher [SendMore] (B.pack channel)
+            send publisher [SendMore] (B.pack "")
             send publisher [] (C.toStrict . encode $ ResponseChannels {channels})
 
           -- Acknowledge receiving the message,
