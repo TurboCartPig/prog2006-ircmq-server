04e9273002a04a25255e98a6427c8e4a4ba531a5
Sebastian Lindtvedt <sebaskl@stud.ntnu.no>
2021-05-08 16:41:05 +0200

    small refactoring in handlers

--- src/Handlers.hs
+++ src/Handlers.hs
@@ -15,11 +15,9 @@ import qualified Data.ByteString.Lazy  as C
 import           Message
 import           System.ZMQ4.Monadic
 
--- | sendPub - function to group together some redundant functionality
---
-
-sendPub :: (Sender t, ToJSON a) => String -> [String] -> [String] -> String -> Socket z t -> a -> ZMQ z ()
-sendPub channel members channels broadcast publisher msgType = do
+-- | publish - function to group together some redundant functionality
+publish :: (Sender t, ToJSON a) => String -> [String] -> [String] -> String -> Socket z t -> a -> ZMQ z ()
+publish channel members channels broadcast publisher msgType = do
   send publisher [SendMore] (B.pack channel)
   send publisher [] (C.toStrict . encode $ ResponseMembers {members})
   send publisher [SendMore] (B.pack broadcast)
@@ -27,32 +25,27 @@ sendPub channel members channels broadcast publisher msgType = do
   send publisher [SendMore] (B.pack channel)
   send publisher [] (C.toStrict . encode $ msgType)
 
--- | mHello - first message from the client
---
+-- | hello - first message from the client
 -- Acknowledges the message and adds them to channel participants
---
 hello :: (Sender t1, Sender t2) => String -> String -> Socket z t1 -> Socket z t2 -> ChannelParticipants -> ZMQ z ()
 hello name channel responder publisher channels = do
   send responder [] "ACK"
   liftIO $ insertChannelParticipant channels name channel
   members <- liftIO (fetchChannelParticipants channels channel)
   channels <- liftIO (fetchAllChannelNames channels)
-  sendPub channel members channels broadcast publisher $ Hello { name, channel }
+  publish channel members channels broadcast publisher $ Hello { name, channel }
 
--- | mGoodbye - final message from the client.
---
+-- | hello - final message from the client.
 -- Acknowledges the message and removes them from the channel participants.
--- 
 goodbye :: (Sender t1, Sender t2) => String -> String -> Socket z t1 -> Socket z t2 -> ChannelParticipants -> ZMQ z ()
 goodbye name channel responder publisher channels = do
   send responder [] "ACK"
   liftIO $ removeChannelParticipant channels name channel
   members <- liftIO (fetchChannelParticipants channels channel)
   channels <- liftIO (fetchAllChannelNames channels)
-  sendPub channel members channels broadcast publisher $ Goodbye { name, channel }
+  publish channel members channels broadcast publisher $ Goodbye { name, channel }
 
--- | mMessage - Acknowledges receiving the message and passes it to all clients
---
+-- | message - Acknowledges receiving the message and passes it to all clients
 message :: (Sender t1, Sender t2) => [Char] -> String -> [Char] -> Socket z t1 -> Socket z t2 -> ZMQ z ()
 message name channel content responder publisher = do
   send responder [] "ACK"
@@ -61,8 +54,7 @@ message name channel content responder publisher = do
   send publisher [SendMore] (B.pack channel)
   send publisher [] (C.toStrict . encode $ Message { name, channel, content })
 
--- | mReqMembers - gives the client a list of all channel participants
---
+-- | reqMembers - gives the client a list of all channel participants
 reqMembers :: Sender t => String -> ChannelParticipants -> Socket z t -> ZMQ z ()
 reqMembers channel channels responder = do
   members <- liftIO (fetchChannelParticipants channels channel)
